34ead104937685c0d0c6925d10c173dd1cb142ef
ВоробьевКЕ <Meserygamer@gmail.com>
2023-05-20 08:41:44 +0300

    Добавлен класс для метода Монте-Карло

---
+++ MethodMonteCarlo/MonteCarloMethod.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MethodMonteCarlo
+{
+    public class MonteCarloMethod
+    {
+        private static MonteCarloMethod singletone;
+        private MonteCarloMethod(){}
+        public static MonteCarloMethod GetObject()
+        {
+            if(singletone is null)
+            {
+                singletone = new MonteCarloMethod();
+                return singletone;
+            }
+            else
+            {
+                return singletone;
+            }
+        }
+    }
+}
