82706c79d3dbebae1f66bb5e70b9dccb97e58cde
ВоробьевКЕ <Meserygamer@gmail.com>
2023-05-20 11:19:15 +0300

    Добавлена задача 6 по расчету площади

--- MethodMonteCarlo/MonteCarloMethod.cs
+++ MethodMonteCarlo/MonteCarloMethod.cs
@@ -106,5 +106,17 @@ namespace MethodMonteCarlo
             }
             return (k / NumIter) * 32;
         }
+        public double CalculateSplashZad6(double NumIter)
+        {
+            Random r = new Random();
+            double k = 0;
+            for (int i = 0; i < NumIter; i++)
+            {
+                double x = (r.NextDouble() * 3);
+                double y = (r.NextDouble() * 1);
+                if (y >= (x - 2) * (x - 2) / 2 && y <= Math.Sin(x)) k++;
+            }
+            return (k / NumIter) * 3;
+        }
     }
 }
--- MethodMonteCarlo/Program.cs
+++ MethodMonteCarlo/Program.cs
@@ -5,19 +5,30 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Введите количество итераций при подсчете числа пи");
-            Console.WriteLine((double)(MonteCarloMethod.GetObject().CalculatePi(Convert.ToInt32(Console.ReadLine()))));
+            int r = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Результат расчета числа ПИ:");
+            Console.WriteLine((double)(MonteCarloMethod.GetObject().CalculatePi(r)));
             //Console.WriteLine((double)(MonteCarloMethod.GetObject().CalculateSplashExample(Convert.ToInt32(Console.ReadLine()))));
+            Console.WriteLine("Результат расчета примера:");
             Console.WriteLine((double)(MonteCarloMethod.GetObject().CalculateSplashExample(1000000)));
+            Console.WriteLine("Результат задачи 1:");
             //Console.WriteLine((double)(MonteCarloMethod.GetObject().CalculateSplashZad1(Convert.ToInt32(Console.ReadLine()))));
             Console.WriteLine((double)(MonteCarloMethod.GetObject().CalculateSplashZad1(10000000)));
+            Console.WriteLine("Результат задачи 2:");
             //Console.WriteLine((double)(MonteCarloMethod.GetObject().CalculateSplashZad2(Convert.ToInt32(Console.ReadLine()))));
             Console.WriteLine((double)(MonteCarloMethod.GetObject().CalculateSplashZad2(10000000)));
+            Console.WriteLine("Результат задачи 3:");
             //Console.WriteLine((double)(MonteCarloMethod.GetObject().CalculateSplashZad3(Convert.ToInt32(Console.ReadLine()))));
             Console.WriteLine((double)(MonteCarloMethod.GetObject().CalculateSplashZad3(10000000)));
+            Console.WriteLine("Результат задачи 4:");
             //Console.WriteLine((double)(MonteCarloMethod.GetObject().CalculateSplashZad4(Convert.ToInt32(Console.ReadLine()))));
             Console.WriteLine((double)(MonteCarloMethod.GetObject().CalculateSplashZad4(10000000)));
+            Console.WriteLine("Результат задачи 5:");
             //Console.WriteLine((double)(MonteCarloMethod.GetObject().CalculateSplashZad5(Convert.ToInt32(Console.ReadLine()))));
             Console.WriteLine((double)(MonteCarloMethod.GetObject().CalculateSplashZad5(10000000)));
+            Console.WriteLine("Результат задачи 6:");
+            //Console.WriteLine((double)(MonteCarloMethod.GetObject().CalculateSplashZad6(Convert.ToInt32(Console.ReadLine()))));
+            Console.WriteLine((double)(MonteCarloMethod.GetObject().CalculateSplashZad6(10000000)));
         }
     }
 }
